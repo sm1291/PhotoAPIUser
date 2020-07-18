f3434a7138eb71e3fe37bbdec2d7bc5cf9d946cf
Sai Krishna <mskrishna1122@gmail.com>
2020-07-18 16:18:05 -0400

    Config Server

---
+++ PhotoAppApiConfigServer/src/test/java/io/sk/photoapp/api/PhotoAppApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sk.photoapp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
